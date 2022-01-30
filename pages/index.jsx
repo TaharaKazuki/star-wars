bc7bf69253c36722be3492ff4e979531aee5bca8
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-01-30 22:59:08 +0900

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,29 @@
 import styled from 'styled-components'
+import Head from 'next/head'
 
-export default function Home() {
+export default function Characters({ characters }) {
   return (
     <>
-      <h1>Starwars</h1>
+      <Head>
+        <title>Star Wars Characters | StarWars.com</title>
+      </Head>
+      <HomeScreenContainer>
+        <h1>Starwars</h1>
+      </HomeScreenContainer>
     </>
   )
 }
+
+export async function getStaticProps(context) {
+  const characters = await fetch(
+    'https://akabab.github.io/starwars-api/api/all.json'
+  ).then((res) => res.json())
+
+  return {
+    props: {
+      characters,
+    },
+  }
+}
+
+const HomeScreenContainer = styled.div``
